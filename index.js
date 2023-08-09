48f20b284f7cff0b5b05814a090c9eae593d9022
Bruno Pedroso <bpedrososofteng@gmail.com>
2023-08-08 21:46:21 -0300

    Melhorando o código

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const axios = require("axios");
 
 const server = express();
 
-server.get("/Awari", async (req,res)=> {
-    const result = await axios.get(`https://viacep.com.br/ws/${req.query.cep}/json`);
-    return res.send(result.data);
+server.get("/Awari", async (req, res) => {
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws/${req.query.cep}/json`);
+        const addressData = response.data;
+        return res.json(addressData);
+    } catch (error) {
+        return res.status(500).json({ error: "Error fetching address data" });
+    }
 });
 
-server.listen(3333);
+const PORT = 3333;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
